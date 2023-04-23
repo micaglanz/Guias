2e645e084a0260671b6ff7ca09c8d4d8b6076ef5
micaglanz <129191141+micaglanz@users.noreply.github.com>
2023-04-23 20:15:22 -0300

    Update guia5.hs

--- guia5.hs
+++ guia5.hs
@@ -1,3 +1,57 @@
+--ej 1. Generar una lista infinita de unos
+listUno :: [Integer]
+listUno = 1:listUno
+
+--ej 2. Generar una lista infinita de naturales comenzando desde un numero dado.
+
+listNat :: Int -> [Int]
+listNat n = [n,n+1..]
+
+--ej 3. Generar una lista con los primeros n naturales.
+
+primerosNat :: Int -> [Int]
+primerosNat n = [1..n]
+
+--ej 4. Retornar los primeros 5 elementos de una lista infinita de enteros positivos.
+
+primeros5Nat :: [Int] -> [Int]
+primeros5Nat xs = take 5 xs           --preguntar como se compila en la terminal, debo citar a xs como una lista inf de enteros? si es asi, Como lo hago?
+
+
+
+
+--*************************************************************** FUNCIONES DE ALTO ORDEN ****************************************************************
+
+-- ej 5. Dada una lista de enteros, retornar sus cuadrados
+
+listCuad :: [Int] -> [Int]
+listCuad xs = map (^2) xs 
+
+--ej 6. Dado un entero positivo, retornar la lista de sus divisores.
+
+         -- divs es funcion auxiliar
+divs :: Int -> Int -> Bool
+divs x y = (mod x y ) == 0
+
+divisores :: Int -> [Int] 
+divisores n = filter (divs n) [1..n] -- el divs n funciona haciendo divs n 1 luego, divs n 2 y asi sucesivamente hasta llegar a divs n n. luego el filter se queda con los casos verdaderos 
+
+
+--ej 7. Dada una lista de naturales, obtener la lista que contenga solo los numeros primos de la lista original.
+
+   --soyPrimo es funcion auxiliar 
+soyPrimo :: Int -> Bool
+soyPrimo 1 = False
+soyPrimo n = and [(mod n i) /= 0 | i<- [2..(n-1)]]
+
+
+listPrimos :: [Int] -> [Int]
+listPrimos xs = filter (soyPrimo) xs
+
+--ej 8. Dada una lista de naturales, retornar la suma de los cuadrados de la lista.
+
+sumCuadList :: [Int] -> Int 
+sumCuadList xs = foldr (+) 0 (map (^2) xs)
 -- ej9. Dada una lista de naturales, retornar la lista con sus sucesores.
 listSucc :: [Int] -> [Int]
 listSucc xs = map (+1)  xs
@@ -82,7 +136,12 @@ productList xs ys = [ (x,y) | (x<-xs), (y<-ys) ]
 --ej21. Dadas una lista y un elemento retornar el numero de ocurrencias del elemento x en la lista ys.
 
 ocurrencias :: Int -> [Int] -> Int
-ocurrencias n xs = [x| x<xs, x==n]
+ocurrencias n xs = length ([x| x<-xs, x==n])
+
+--ej 22. Escribir la funcion split2 :: [a] - > [([a],[a])], que dada una lista xs, devuelve la lista con todas las formas de partir xs en dos. Por ejemplo: split2 [1,2,3] = [([],[1,2,3]), ([1],[2,3]), ([1,2],[3]),([1,2,3],[])].
+
+split2 :: [a] -> [([a],[a])]
+split2 xs = []
 
 --ej 18, 22, 23 son tipo parcial, averiguar que es nub en haskell
 
