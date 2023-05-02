cc9a3819fa98f638ce28ac1c68dd7cbd9e51ff12
Micaela Glanzmann <129191141+micaglanz@users.noreply.github.com>
2023-05-01 21:06:45 -0300

    Add files via upload

---
+++ parcial2022.hs
@@ -0,0 +1,45 @@
+                                                     --EJERCICIO 1--
+{-Dado un arbol binario de enteros (de la forma que fueron definidos en clases), definir una funcion en Haskell f:BinTree Int -> Int que retorne la suma de la informacion en los nodos.
+
+   Escriba un arbol que contenga los 4 primeros digitos de su DNI.
+   
+-}  
+
+
+                                                     --EJERCICIO 2--
+{-Dado el siguiente tipo de datos definidos en Haskell: data SIntExp = Cte Int | Sum SIntExp SIntExp | Mul SIntExp SIntExp
+       
+     Dar 4 expresiones que sean del tipo dado.
+     Con el tipo dado dar una expresion correspondiente a d0 + (d1 ∗ d2). En donde d0, d1, d2 son los tres primeros digitos de su DNI.
+-}     
+
+
+                                                      --EJERCICIO 3-- 
+{-Defina la funcion compact:: [Int] -> [Int] que elimina repeticiones consecutivas de un elemento. Por ejemplo: compact [2,2,3,1,3] = [2,3,1,3]. Aplique la funcion a los digitos de su DNI, y muestre las reducciones que harıa Haskell.
+-}
+
+
+                                                      --EJERCICIO 4--
+
+{-
+Dadas las siguientes definiciones:
+
+square :: Int -> Int
+square x = x*x
+
+and :: Bool -> Bool -> Bool
+and true y = y
+and false x = false
+
+inf :: Int -> Int
+inf = inf+1
+
+Elija solo dos de las siguientes expresiones y aplique las eveluaciones normal y aplicativa.
+Cuales de ellas tienen formal normal y cuales no? Justificar su respuesta.
+
+(square inf) + (square inf)
+and (inf == inf) ((square 2) == 4)
+and ((square 2) == 5) (inf == inf)
+
+-}
+
